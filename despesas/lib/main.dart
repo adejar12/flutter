0d23d524330a2a10793b5a2941dbf29f2190e7ed
adejar12 <adejarjunior18@gmail.com>
2022-05-16 20:36:14 -0300

    feat: starting a new project

---
+++ despesas/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(
+        child: Text('Versão Inicial'),
+      ),
+    );
+  }
+}
