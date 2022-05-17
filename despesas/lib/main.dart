a838451d9600be56584c6f4442119619fa9f91cf
adejar12 <adejarjunior18@gmail.com>
2022-05-16 22:07:34 -0300

    style: styling the column card

--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -53,15 +53,34 @@ class MyHomePage extends StatelessWidget {
                       margin:
                           EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                       decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2),
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
                       ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(tr.value.toString()),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        tr.value.toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
                     ),
-                    Column(children: [
-                      Text(tr.title),
-                      Text(tr.date.toString()),
-                    ]),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tr.title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              )),
+                          Text(tr.date.toString(),
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              )),
+                        ]),
                   ]),
                 );
               }).toList(),
