e4f71ea937232769808103f7fc1745eea2e3698d
adejar12 <adejarjunior18@gmail.com>
2022-05-23 20:21:55 -0300

    feat: adding textField

--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -35,58 +35,79 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              child: Card(
-                color: Colors.blue,
-                child: Text('Grafico'),
-                elevation: 5,
-              ),
-            ),
-            Column(
-              children: _transactions.map((tr) {
-                return Card(
-                  child: Row(children: [
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        Container(
+          child: Card(
+            color: Colors.blue,
+            child: Text('Grafico'),
+            elevation: 5,
+          ),
+        ),
+        Column(
+          children: _transactions.map((tr) {
+            return Card(
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(tr.title,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              )),
-                          Text(DateFormat('d MMM y').format(tr.date),
-                              style: const TextStyle(
-                                color: Colors.grey,
-                              )),
-                        ]),
-                  ]),
-                );
-              }).toList(),
-            )
-          ]),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(tr.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      )),
+                  Text(DateFormat('d MMM y').format(tr.date),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      )),
+                ]),
+              ]),
+            );
+          }).toList(),
+        ),
+        Card(
+          elevation: 5,
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            child: Column(children: [
+              const TextField(
+                decoration: InputDecoration(labelText: 'Título'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Valor (R\$)'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Nova Transação'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.purple,
+                      onPrimary: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        )
+      ]),
     );
   }
 }
