f0c03af68a844b28fe413f8409049a23331f9240
adejar12 <adejarjunior18@gmail.com>
2022-05-23 22:20:57 -0300

    feat: change SingleChildScrollView to listView

--- despesas/lib/components/transaction_list.dart
+++ despesas/lib/components/transaction_list.dart
@@ -4,51 +4,50 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-
   const TransactionList({required this.transactions});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(children: [
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
                   ),
                 ),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Text(tr.title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      )),
-                  Text(DateFormat('d MMM y').format(tr.date),
-                      style: const TextStyle(
-                        color: Colors.grey,
-                      )),
-                ]),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text(tr.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    )),
+                Text(DateFormat('d MMM y').format(tr.date),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    )),
               ]),
-            );
-          }).toList(),
-        ),
+            ]),
+          );
+        },
       ),
     );
   }
